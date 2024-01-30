45f6c48c3463d657461a6e9e6343af35336ffe5e
Tristan Budiman <29161072+trisbman@users.noreply.github.com>
2024-01-31 01:57:30 +0700

    tweak(membership-check): minor changes

--- pages/check-membership.vue
+++ pages/check-membership.vue
@@ -51,8 +51,8 @@ export default {
 <template>
   <main>
     <div class="max-w-[calc(384px+16px)] mx-auto mt-12 px-4">
-      <h2 class="text-2xl font-bold mb-8">Check Membership</h2>
-      <label for="price" class="block text-sm font-medium leading-6 text-gray-900">NIM</label>
+      <h2 class="text-2xl font-bold mb-8 text-center">Check Membership</h2>
+      <label for="price" class="block text-sm font-medium leading-6 text-gray-900" style="display: none;">NIM</label>
       <div class="relative mt-2 rounded-md shadow-sm">
         <input type="text" name="price" id="price"
           class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
